91fbdfa0ddc6474e9f023ca93154023004cfd694
Heutlett <44324449+Heutlett@users.noreply.github.com>
2020-07-03 23:22:07 -0600

    .

---
+++ UnityProject/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void playLevel1()
+    {
+
+        Program.Start();
+        Program.sendMessage("play_level01");
+        Debug.Log("Recibiendo el mapa a cargar desde el servidor..");
+        string mensaje = Program.receiveMessage();
+
+        GameManager.setMapData(mensaje);
+        SceneManager.LoadScene(2);
+
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
